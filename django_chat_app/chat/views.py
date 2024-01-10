d803eea4b74c195d851db2d0e517a85af8525138
woftschik <waldemar.matthies@gmx.de>
2024-01-10 15:41:33 -0300

    add register and logout

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,8 +1,10 @@
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Chat, Message
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 @login_required(login_url='/login/')
 def index(request):
@@ -14,6 +16,7 @@ def index(request):
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
 def login_view(request):
+    print("TEST LOGIN")
     redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
@@ -24,3 +27,30 @@ def login_view(request):
         else:
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
+
+def register_view(request):
+    if request.method == 'POST':
+        userName = request.POST.get('username', None)
+        userPass = request.POST.get('password', None)
+        userPassConfirm = request.POST.get('password-confirm', None)
+        userMail = request.POST.get('email', None)
+
+        if userName and userPass and userPassConfirm and userPass == userPassConfirm:
+            try:
+                newUser = User.objects.create_user(username=userName, email=userMail, password=userPass)
+                if newUser:
+                    return redirect('/chat/')
+                else:
+                    return render(request, 'auth/register.html')
+
+            except Exception as e:
+                return render(request, 'auth/register.html', {'message': str(e)})
+        else:
+            print('test-3')
+            return render(request, 'auth/register.html', {'wrongInsert': True})
+
+    return render(request, 'auth/register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/chat/')
--- django_chat_app/django_chat_app/urls.py
+++ django_chat_app/django_chat_app/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from chat.views import index, login_view
+from chat.views import index, login_view, register_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', index),
     path('login/', login_view),
+    path('register/', register_view),
+    path('logout/', logout_view, name='logout'),
 ]
