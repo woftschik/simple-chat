1f5d93f59713a34fb4d8ae6b0224375a45dfb608
woftschik <waldemar.matthies@gmx.de>
2024-01-12 10:19:44 -0300

    add json request

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -5,18 +5,22 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.http import JsonResponse
+from django.core import serializers
 
 @login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print("Received data " + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [new_message, ])
+        #return JsonResponse(serialized_obj[1:-1], safe=False)
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
+
 def login_view(request):
-    print("TEST LOGIN")
     redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
@@ -27,6 +31,7 @@ def login_view(request):
         else:
             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -42,14 +47,12 @@ def register_view(request):
                     return redirect('/chat/')
                 else:
                     return render(request, 'auth/register.html')
-
             except Exception as e:
                 return render(request, 'auth/register.html', {'message': str(e)})
         else:
-            print('test-3')
             return render(request, 'auth/register.html', {'wrongInsert': True})
-
     return render(request, 'auth/register.html')
+
 
 def logout_view(request):
     logout(request)
